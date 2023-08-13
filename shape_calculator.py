ce51232e857e623654ce42215b873a475f493d32
Nick Le Large <nick@le-large.com>
2023-08-13 23:31:08 +0200

    implement rectangle and square

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,49 @@
+import math
+
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def set_width(self, width):
+        self.width = width
 
+    def set_height(self, height):
+        self.height = height
 
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+    
+    def get_picture(self):
+        if self.height > 50 or self.width > 50:
+            return "Too big for picture."
+        return self.height * (self.width * "*" + "\n")
+    
+    def get_amount_inside(self, shape):
+        return math.floor(self.get_area() / shape.get_area())
+    
+    def __str__(self):
+        return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
 
-class Square:
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.set_side(height)
+
+    def __str__(self):
+        return "Square(side=" + str(self.width) + ")"
